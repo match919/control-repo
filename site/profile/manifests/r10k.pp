ecc1d4801dd470d05980f4c5dab0da59650f0210
match919 <91558461+match919@users.noreply.github.com>
2023-10-04 13:20:52 -0700

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,9 +1,22 @@
 class profile::r10k {
   class {'r10k':
-    remote => 'https://github.com/match919/control-repo/',
+    version => '1.5.1',
+    sources => {
+     'puppet' => {
+        'remote' => 'https://github.com/match919/control-repo/',
+        'basedir' => "${::settings::confdir}/environments",
+        'prefix'  => false,
+     },
+    },
+    manage_modulepath => false
+  }
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl => false,
   }
   class {'r10k:webhook':
     user  => 'root',
-    group => 'root',
-  }  
+    group => '0',
+  }
+  Class['r10k::webhook::config'] -> Class['r10k::webhook']
 }
