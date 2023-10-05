dd8d561d5537b6c13ab6989aefb96b1ba7db9abe
Mark Matchynski <mmatchyn@qti.qualcomm.com>
2023-10-05 08:35:06 -0700

    add file with secrect text
    Change-Id: I735e1987c8655d4ba6c7a213a736b0127c9af182

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,10 @@ node puppet.local {
 }
 node puppet.qualcomm.com {
   include role::master
+  file {'/etc/secret_password.txt':
+    ensure => file,
+    content => lookup('secret_password'),
+  }
 }
 node default {
   include role::master
